4ed56c056168d5120cdf21af50dc207455e33620
kbkn3 <ken0421wabu@gmail.com>
2025-04-19 03:23:14 +0900

    Initial Commit

---
+++ SonoHabit/SonoHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonoHabitApp.swift
+//  SonoHabit
+//
+//  Created by 小林建太 on 2025/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SonoHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SonoHabitTests/SonoHabitTests.swift
@@ -0,0 +1,16 @@
+//
+//  SonoHabitTests.swift
+//  SonoHabitTests
+//
+//  Created by 小林建太 on 2025/04/19.
+//
+
+import Testing
+
+struct SonoHabitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
